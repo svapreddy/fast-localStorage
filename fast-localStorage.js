850d19ee40ee628353e931b977787be35b43a888
Prathap Reddy <svap.reddy@hotmail.com>
2014-10-25 05:24:05 +0530

    Update fast-localStorage.js

--- fast-localStorage.js
+++ fast-localStorage.js
@@ -2,19 +2,26 @@
  
 var LStorage = (function () {
 	function LStorage() {
-		this.localStorage = JSON.parse(JSON.stringify(localStorage));
+		this.init = function(){
+			if(!this.localStorage) {
+				this.localStorage = JSON.parse(JSON.stringify(localStorage));
+			}
+		}
 		// or new Function("return JSON.parse('" + JSON.stringify(localStorage) + "')")(); 
 	}
 	LStorage.prototype.setItem = function (key, val) {
+		this.init();
 		if(this.localStorage[key] == val) return;
 		this.localStorage[key] = val;
 		localStorage.setItem(key, val);
 	};
 	LStorage.prototype.getItem = function (key, undef) {
+		this.init();
 		var val = this.localStorage[key];
 		return val;
 	};
         LStorage.prototype.removeItem = function (key) {
+		this.init();
 		delete this.localStorage[key];
 		localStorage.removeItem(key);
 		return true;
